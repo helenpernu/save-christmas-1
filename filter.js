5ee7e2ee48cb617980286d40a6a6429248f3f519
Helen <helen.pernu@skola.botkyrka.se>
2020-11-30 15:05:22 +0100

    steg 4

--- filter.js
+++ filter.js
@@ -1,6 +1,12 @@
 
 function filter(arr, func) {
-
+    let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i]) === true) {
+            newArray.push(arr[i])
+        }
+    }
+    return newArray
 }
 
 // TEST 1
@@ -9,6 +15,8 @@ const filteredNumbers = filter(numbers, x => x < 10)
 console.log(filteredNumbers)
 
 // TEST 2 - skriv själv
-
+const stringArray = ["Kiwi", "Äpple", "Banan", "Päron"]
+const newArray = filter(stringArray, x => x.length < 5)
+console.log(newArray)
 
 // TEST 3 - skriv själv
