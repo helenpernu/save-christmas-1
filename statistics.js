7283a36aac0669de60521a5fcbfb9ed2c02aaa56
Helen <helen.pernu@skola.botkyrka.se>
2020-11-30 14:32:49 +0100

    steg 2

--- statistics.js
+++ statistics.js
@@ -1,12 +1,34 @@
 
 function max(arr) {
-
+    let largest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            largest = arr[i]
+        }
+    }
+    return largest
 }
 
 function min(arr) {
-
+    let smallest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i]
+        }
+    }
+    return smallest
 }
 
 function mean(arr) {
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        result = result + arr[i]
+    }
+    return result/arr.length
     // Ledtråd: Börja med att räkna ut summan av alla element
 }
+
+const array = [7, 5, 2, 3, 6, 8]
+console.log(max(array))
+console.log(min(array))
+console.log(mean(array))
